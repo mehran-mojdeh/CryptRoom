9e969e4d2b543696ec9ca918c7080b24329dfb47
Mehran Mojdeh <mehran.mojdeh@yahoo.com>
2022-09-28 07:05:02 +0330

    added some basic packages

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello, world!");
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+const { PORT } = process.env;
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//API Routes//
+app.get('/room', async(req, res) => {
+  res.status(200).json({message: 'Hi room!'})
+})
+
+// views
+// app.use(express.static('views'));
+
+
+app.listen(port = PORT||3000, () => {
+  console.log(`server started listening on port ${port}.`);
+})
