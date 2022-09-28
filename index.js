07394206b894fa75af670475a25fb7538ef5977b
Mehran Mojdeh <mehran.mojdeh@yahoo.com>
2022-09-28 17:24:25 +0330

    seprated service routes

--- index.js
+++ index.js
@@ -6,22 +6,14 @@ const { PORT } = process.env;
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-const Messages = require('./services/messages');
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 //API Routes//
-app.get('/messages', async(req, res) => {
-  const messages = await Messages.getMessages()
-  .then(d => d)
-  .catch(e => console.error(e));
-  // formating data
-  const data = messages.map(m => {return { 'message': m[0], 'signiture': m[1]}});
-  res.status(200).json({data})
-});
+const services = require('./services');
+app.use('/api', services);
 
 // views
 // app.use(express.static('views'));
---
+++ services/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+
+const Messages = require('./messages');
+
+app.get('/messages', async(req, res) => {
+  const messages = await Messages.getMessages()
+  .then(d => d)
+  .catch(e => console.error(e));
+  // formating data
+  const data = messages.map(m => {return { 'message': m[0], 'signiture': m[1]}});
+  res.status(200).json({data})
+});
+
+module.exports = router
