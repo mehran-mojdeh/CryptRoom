5477bfed721a9cc153cdabb0dfa7bb5708e7931b
Mehran Mojdeh <mehran.mojdeh@yahoo.com>
2022-09-28 19:13:07 +0330

    getMessages ordered by date

--- services/messages/index.js
+++ services/messages/index.js
@@ -2,7 +2,7 @@ const _query = require('../../db');
 
 module.exports = {
   getMessages: async() => {
-    const res = await _query('SELECT name, msg, sign FROM message',[])
+    const res = await _query('SELECT name, msg, sign FROM message ORDER BY "date"',[])
     .then(d => d)
     .catch(e=>{
       throw new Error(e);
