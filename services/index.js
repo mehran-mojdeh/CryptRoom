77ccf03227782496a8e810a48ad973b66e483d40
Mehran Mojdeh <mehran.mojdeh@yahoo.com>
2022-11-06 07:36:52 +0330

    lastId implemented.

--- services/index.js
+++ services/index.js
@@ -3,16 +3,28 @@ const router = express.Router()
 
 const Messages = require('./messages');
 
+let last;
+
+function generateLast() {
+  last = Math.floor( Math.random() * 1000 );
+}
+generateLast();
+
 router.get('/messages', async(req, res) => {
+  if (req.query.l == last) {
+    res.status(204).send();
+    return;
+  }
   const messages = await Messages.getMessages()
   .then(d => d)
   .catch(e => {
     console.error(e);
     res.status(500).send();
+    throw e;
   });
   // formating data
   const data = messages.map(m => {return {'name':m[0], 'message': m[1], 'signiture': m[2]}});
-  res.status(200).json({data})
+  res.status(200).json({data, last})
 });
 
 router.post('/messages', async(req, res) => {
@@ -39,7 +51,10 @@ router.post('/messages', async(req, res) => {
   }
 
   await Messages.sendMessage(name, message, signiture)
-  .then(d => res.status(200).json(d))
+  .then((d) => {
+    generateLast();
+    res.status(200).json({data: d, last})
+  })
   .catch(e => {
     console.error(e);
     res.status(500).send();
